4bcd560fb26f70086f41fa1e24749a84f11d7152
champ218316@gmail.com <champ218316@gmail.com>
2020-03-25 15:10:32 +0700

    test

--- src/main/java/com/icd/wksh/WorkShop01Application.java
+++ src/main/java/com/icd/wksh/WorkShop01Application.java
@@ -10,7 +10,16 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={RestTemplateAutoConfiguration.class})
@@ -19,6 +28,30 @@ public class WorkShop01Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(WorkShop01Application.class, args);
+		try (Stream<Path> walk = Files.walk(Paths.get("F:\\BackUp\\DB2_DUMP\\sql developer\\DATA_SEMICOLON_DELIMETER"))) {
+			List<String> result = walk.filter(Files::isRegularFile)
+					.map(x -> x.toString()).collect(Collectors.toList());
+
+//			result.forEach(System.out::println);
+			Charset charset = StandardCharsets.UTF_8;
+			result.forEach(pathValue -> {
+				try {
+					log.debug("start replace: filename= {}", pathValue);
+					Path path = Paths.get(pathValue);
+					String content = new String(Files.readAllBytes(path), charset);
+//					content = content.replaceAll("\n", ";");
+//					content = content.replaceAll(";INSERT", "INSERT");
+					content = content.substring(0, content.length()-1);
+					Files.write(path, content.getBytes(charset));
+					log.debug("================ end replace ======================");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
